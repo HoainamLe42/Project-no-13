396d0b60cf83c037ba6d51e2cd7fcb59406193f2
Le Hoai Nam <lehoainam0403@HoaiNam-Le.local>
2024-03-15 08:31:30 +0900

    Làm phần about-blog #1

---
+++ assets/js/feedback.js
@@ -0,0 +1,52 @@
+// Feedback;
+
+// const listFeedback = document.querySelector('.user-feedback__list');
+// const items = document.querySelectorAll('.user-feedback__item');
+// const itemLength = items.length;
+// const btnLeft = document.querySelector('.user-feedback__btt--left');
+// const btnRight = document.querySelector('.user-feedback__btt--right');
+
+// const handleChangeSlide = function () {};
+
+// let current = 0;
+// setInterval(() => {
+//   if (current == itemLength - 1) {
+//     current = 0;
+//     let width = items[0].offsetWidth;
+
+//     listFeedback.style.transform = `translateX(0px)`;
+//   } else {
+//     current++;
+//     let width = items[0].offsetWidth;
+
+//     listFeedback.style.transform = `translateX(${width * -current}px)`;
+//   }
+// }, 4000);
+
+// btnLeft.addEventListener('click', function () {
+//   if (current == itemLength - 1) {
+//     current = 0;
+//     let width = items[0].offsetWidth;
+
+//     listFeedback.style.transform = `translateX(0px)`;
+//   } else {
+//     current++;
+//     let width = items[0].offsetWidth;
+
+//     listFeedback.style.transform = `translateX(${width * -current}px)`;
+//   }
+// });
+
+// btnRight.addEventListener('click', function () {
+//   if (current == itemLength - 1) {
+//     current = 0;
+//     let width = items[0].offsetWidth;
+
+//     listFeedback.style.transform = `translateX(0px)`;
+//   } else {
+//     current++;
+//     let width = items[0].offsetWidth;
+
+//     listFeedback.style.transform = `translateX(${width * -current}px)`;
+//   }
+// });
